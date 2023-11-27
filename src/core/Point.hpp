cf0a88a2d4947bf3df31c9eb372dc31450deeb7b
CodeforEvolution <secundaja@gmail.com>
2023-11-27 15:31:23 -0600

    Point2D: Introduce class for clear point handling

---
+++ src/core/Point.hpp
@@ -0,0 +1,18 @@
+// Assignment 2a - Programming Interactive 2D Graphics with OpenGL
+// Work by Jacob Secunda
+#ifndef ASSIGNMENT2A_POINT_HPP
+#define ASSIGNMENT2A_POINT_HPP
+
+template<typename T>
+struct Point2D
+{
+	T x;
+	T y;
+};
+
+using Point2Di = Point2D<int>;
+using Point2Df = Point2D<float>;
+
+
+
+#endif //ASSIGNMENT2A_POINT_HPP
